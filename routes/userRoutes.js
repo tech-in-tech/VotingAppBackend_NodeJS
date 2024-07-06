68f9e434b65915363f7f7d96ddd20a5b15e81b43
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-07-06 22:42:27 +0530

    did some debuging in user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const User = require("./../models/user.model");
 const { jwtAuthMiddleware, generateToken } = require('./../jwt');
 
-// ? post route to add a User
+// ! post route to add a User
 router.post('/signup', async (req, res) => {
   try {
-    const data = req.body; // ! assuming the request body contains the user data
-    //! Create newUser document using the Mongoose model
+    const data = req.body; // * assuming the request body contains the user data
+    //* Create newUser document using the Mongoose model
     const newUser = new User(data);
 
-    // !save the new user to the data base
+    // *save the new user to the data base
     const response = await newUser.save();
     console.log('data Saved');
 
@@ -27,3 +27,68 @@ router.post('/signup', async (req, res) => {
     res.status(500).json({ error: "INTERNAL SERVER ERROR" });
   }
 })
+
+// ! Login route
+router.post('/login',async(req,res)=>{
+  try {
+    // *Extract aadharCardNumber and password from request body
+    const {aadharCardNumber,password} = req.body;
+    // *find the user by aadharCardNumber
+    const user = await User.findOne({aadharCardNumber:aadharCardNumber});
+
+    // *if user does not exist or password does not match, return error
+    if(!user || !(await user.comparePassword(password))){
+      return res.status(401).json({error:"Invalid username or password"});
+    }
+    // *generate Token
+    const payload = {
+      id:user.id,  
+    }
+    const token = generateToken(payload);
+    res.json({token})
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({error:"INTERNAL SERVER ERROR"})
+  }
+})
+
+// !Profile route
+router.get("/profile",jwtAuthMiddleware,async (req, res) => {
+  try {
+    const userData = req.user;
+    const userId= userData.id;
+    const user = await User.findById(userId);
+    res.status(200).json({user});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error:"INTERNAL SERVER ERROR"});
+  }
+})
+
+// !change password 
+router.put("profile/password", jwtAuthMiddleware,async(req,res)=>{
+  try {
+    const userId = req.user.id;   //* extract user id fron token
+
+    const {currentPassword,newPassword} = req.body; 
+
+    // *Find the user by userId
+    const user  = await  User.findById(userId);
+    // * if password does not match, return error
+    if(!(await user.comparePassword(currentPassword))){
+      return res.status(401).json({error:"INVALID USERNAME OR PASSWORD"})
+    }
+    // * update the user's password
+    user.password = newPassword;
+    await user.save();
+    console.log("Password updated");
+
+
+    res.status(200).json({message:"Password updated"});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error:"INTERNAL SERVER ERROR"});
+  }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express= require('express')
 const app = new express()
-// require('dotenv').config();
+const db= require('./db')
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())  // req.body
-const PORT  = process.env.port || 3000;
+const PORT  = process.env.PORT  || 3000;
+
+
+// * Import the router files
+const userRoutes = require('./routes/userRoutes');
+
+// *use the routers
+app.use('/user',userRoutes);
 
 app.listen(PORT,()=>{
-  console.log("Listin on port 3000");
+  console.log("Listening on port 3000");
 })
